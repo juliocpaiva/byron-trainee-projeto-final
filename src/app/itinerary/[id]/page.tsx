deae8b065572891bc658cf8c7566b46ef8024c06
Júlio <juliotaek@gmail.com>
2024-03-01 16:00:53 -0300

    Atualização de arquivo it e unsubscribe

--- src/app/itinerary/[id]/page.tsx
+++ src/app/itinerary/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function Itinerary({ params }: { params: { id: string } }) {
     <img src="public/amsterdam.jpg"
      alt="Imagem da cidade de Amsterdam" />
      <h1 className="font-bold justify-center"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, velit?</h1>
-    <p className=" max-w-4xl justify-center  "
+    <p className=" max-w-4xl justify-center  ">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. In ipsum ipsam maiores mollitia eius consequatur, placeat doloribus? Veritatis porro consequatur totam pariatur. Dicta excepturi animi nisi nostrum provident voluptate inventore esse ab sunt ad saepe accusamus mollitia iusto incidunt sit molestiae placeat velit, quod ea recusandae voluptas ex nesciunt hic!
     </p>
   </div>
